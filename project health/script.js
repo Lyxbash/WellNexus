a7a0b1c9954c182eb56c784de865e80343d848c7
Tripti Mahajan <104572363+Lilflerkin@users.noreply.github.com>
2023-04-16 06:12:59 +0530

    Delete project health directory

--- project health/script.js
+++
@@ -1,8 +0,0 @@
-let menubar=document.querySelector('#menubar');
-let mynav=document.querySelector('.navbar');
-
-menubar.onclick=()=>{
-    menubar.classLlist.toggle('fa-times')
-    mynav.classList.toggle('active')
-}
-
